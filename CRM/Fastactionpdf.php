01c1d809a17b7a01d87f7933acb1f2208f205335
Jon Goldberg <jon@megaphonetech.com>
2019-08-01 16:48:16 -0400

    add installer to create the CiviRuleAction; first working draft that generates PDFs

--- CRM/Fastactionpdf.php
+++ CRM/Fastactionpdf.php
@@ -2,12 +2,28 @@
 
 class CRM_Fastactionpdf extends CRM_Pdfapi_Pdf {
   protected function processPdf() {
-    $temp = $this->_subject;
-    CRM_Utils_System::download('TYL', 'application/pdf', file_get_contents($this->_fullPathName));
-  }
+    // Move the file to the custom file upload dir if need be.
+    if (!(isset($this->_apiParams['email_activity']) && $this->_apiParams['email_activity'] == TRUE)) {
+      $config = CRM_Core_Config::singleton();
+      copy($this->_fullPathName, $config->customFileUploadDir . $this->_fileName);
+      $this->_fullPathName = $config->customFileUploadDir . $this->_fileName;
+    }
 
-  protected function sendPdf($email) {
-    return;
+    $mimeType = 'application/pdf';
+    $fileName = $this->_fileName;
+    $fid = parent::createFileForPDF();
+    $url = CRM_Utils_System::url('civicrm/file', "reset=1&filename={$fileName}&mime-type={$mimeType}", TRUE);
+    //We're about to CiviExit(), so let's grab the FAL settings to put into the JSON response.
+    $result = civicrm_api3('FastActionLink', 'getsingle', [
+      'return' => ["success_message", "dim_on_use", "confirm"],
+      'id' => 1,
+    ]);
+    CRM_Core_Page_AJAX::returnJsonResponse([
+      'url' => $url,
+      'success_message' => $result['success_message'],
+      'dim_on_use' => $result['dim_on_use'],
+      'confirm' => $result['confirm'],
+    ]);
   }
 
 }
---
+++ sql/insertFastActionPDFAction.sql
@@ -0,0 +1,2 @@
+INSERT INTO civirule_action (name, label, class_name, is_active)
+VALUES('fast_action_pdf', 'Fast Action PDF', 'CRM_Fastactionpdf_CivirulesAction', 1);
